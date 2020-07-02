1b05974764088d45bb11714ec476a20c6e6810c6
Jeremie Billet <jeremiebillet@orange.fr>
2020-07-02 11:34:19 +0200

    ajout des ctors et getters

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -6,8 +6,42 @@
  */
 public class Utilisateur {
 	
+	/*_________________props____________________*/
 	private int id;
 	private String nom;
 	private String prenom;
+	
+	/*_________________ctors____________________*/
+	public Utilisateur() {
+	}
+
+	/*______________getters/setters________________*/
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+	
+	
 
 }//end class
